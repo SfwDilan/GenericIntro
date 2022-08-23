a9ee145c82deeda5eefbf5ab89677503f7aaaed7
Dilan Budak <dilanbudak04@gmail.com>
2022-08-23 18:37:29 +0300

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using Generic;
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> myList = new MyList<string>();
+
+            myList.Add("Ali");
+            Console.WriteLine(myList.Length);
+            myList.Add("Veli");
+            Console.WriteLine(myList.Length);
+            myList.Add("Can");
+            myList.Add("Deniz");
+
+            Console.WriteLine(myList.Length);
+
+           
+
+            
+        }
+    }
+}
---
+++ Generic/MyList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generic
+{
+    //Kendi List 'emizi yaptık.
+    public class MyList<T>
+    {
+        T[] items;
+        public MyList()
+        {
+            items = new T[0];   //new'lendiği an Listemin 0 elemanınlı olduğunu söylüyorum.
+        }
+
+        public void Add(T item)
+        {
+            T[] tempArray = items;
+            items = new T[items.Length+1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length - 1] = item;
+
+        }
+
+        public int Length 
+        { 
+            get 
+            {
+                return items.Length;
+            } 
+        }
+
+       
+
+    }
+}
